da5fb2993406d421198f40230e678a24bbe96b98
Gabriel Stefanoni <gabrielpstefanoni@gmail.com>
2025-11-12 18:46:42 -0300

    BackEnd added

---
+++ backend/src/main/java/com/acai/pay/AcaipayApplication.java
@@ -0,0 +1,13 @@
+package com.acai.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaipayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaipayApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/acai/pay/AcaipayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acai.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaipayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
